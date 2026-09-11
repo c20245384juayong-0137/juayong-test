b4040825b16e2d14fbd32fbdaa10909dbd573d74
c20245384juayong-0137 <c20245384.juayong@csav.edu.ph>
2026-09-11 10:45:41 +0800

    functions

--- js.js
+++ js.js
@@ -2,9 +2,10 @@ console.log("Hello, world.");
 console.log("First Javascript");
 
 
-const myName = "Philip Alexander P. Juayong",
+const myName = "Philip Alexander Juayong",
     myNumber = 9940369664,
     myAdress = "Hda Sta Teresa, Brgy. Alicante E.B Magalona";
+
 
 let age = 20;
 
@@ -12,3 +13,35 @@ console.log(`Name: ${myName}`);
 console.log(`Age: ${age}`);
 console.log(`Number: ${myNumber}`);
 console.log(`Address: ${myAdress}`);
+
+//Functions
+
+function greet(name) {
+    return `Good Morning, ${name}!`;
+
+}
+
+function mdas(num1, num2, operator,) {
+    switch (operator) {
+        case "+":
+            return num1 + num2;
+        case "-":
+            return num1 - num2;
+        case "*":
+            return num1 * num2;
+        case "/":
+            return num1 / num2;
+        default:
+            return "Invalid operator";
+
+
+    }
+}
+console.log(greet("Rene"));
+
+
+
+console.log("Multiplication:" + mdas(5, 3, "*"));
+console.log("Division:" + mdas(5, 3, "/"));
+console.log("Subtraction:" + mdas(5, 3, "-"));
+console.log("Addition:" + mdas(5, 3, "+"));
