6c0c131b911e73c1238637411079a65d02317661
c20245384juayong-0137 <c20245384.juayong@csav.edu.ph>
2026-09-11 09:21:21 +0800

    Variables (let & const)

--- js.js
+++ js.js
@@ -1,2 +1,14 @@
 console.log("Hello, world.");
 console.log("First Javascript");
+
+
+const myName = "Philip Alexander P. Juayong",
+    myNumber = 9940369664,
+    myAdress = "Hda Sta Teresa, Brgy. Alicante E.B Magalona";
+
+let age = 20;
+
+console.log(`Name: ${myName}`);
+console.log(`Age: ${age}`);
+console.log(`Number: ${myNumber}`);
+console.log(`Address: ${myAdress}`);
