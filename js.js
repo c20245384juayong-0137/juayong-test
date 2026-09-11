956b4f761619a0f4d5041e1678a36b2be3c1f3ad
c20245384juayong-0137 <c20245384.juayong@csav.edu.ph>
2026-09-11 11:27:57 +0800

    textContent

--- js.js
+++ js.js
@@ -16,13 +16,13 @@ console.log(`Age: ${age}`);
 console.log(`Number: ${myNumber}`);
 console.log(`Address: ${myAdress}`);
 
-//Functions
+//Functions is calling a block of code that can be reused
 
 function greet(name) {
     return `Good Morning, ${name}!`;
 
 }
-
+//mdas is the order of operations in math, it stands for Multiplication and Division, Addition and Subtraction
 function mdas(num1, num2, operator,) {
     switch (operator) {
         case "+":
@@ -38,23 +38,32 @@ function mdas(num1, num2, operator,) {
 
 
     }
-}
+}//The output of the function greet is displayed in the console
 console.log(greet("Rene"));
 
 
-
+//The output of the function mdas is displayed in the console
 console.log("Multiplication:" + mdas(5, 3, "*"));
 console.log("Division:" + mdas(5, 3, "/"));
 console.log("Subtraction:" + mdas(5, 3, "-"));
 console.log("Addition:" + mdas(5, 3, "+"));
 
-
+//const is used to select the first element that matches the specified in HTML code selector
 const heading = document.querySelector("h1");
 
+
+//The output of the heading is displayed in the console
 console.log(heading);
 
+
+//const is used to select all elements that matches the specified in HTML code selector
 const main = document.querySelectorAll("h2");
 
-
+//the output of the main is displayed in the console
 console.log(main[1]);
 console.log(main[2]);
+
+// The textContent property is used to set or re assign the value.
+heading.textContent = "My Portfolio";
+main[1].textContent = "Let's Connect!";
+main[2].textContent = "My Projects";
